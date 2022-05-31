90192f792e968c0eea3038f9a0c1e4d271046d73
Sachin Singh <sachin11vivaan@gmail.com>
2022-05-31 10:50:46 +0530

    third commit

---
+++ STATC/src/BABA/P.java
@@ -0,0 +1,43 @@
+package BABA;
+final class A
+{
+	int A= 40;
+	
+}
+class P extends A {
+//	static int age;
+	final int speedlimit=90;
+	void run()
+	{
+		//speedlimit=400;
+		
+	}
+//	
+//
+	public static void main(String[] args) {
+		P obj =new P();
+		obj.run();
+
+	}
+}
+//		P ob1 = new P();
+//		P ob2 = new P();
+//		
+//		ob1.age=30;
+//		ob2.age=40;
+//		
+//		System.out.println("age is" + ob1.age);
+//		System.out.println("age is" + ob2.age);
+//	}
+//}
+//
+//
+//
+// 
+//		
+//				
+//		// TODO Auto-generated method stub
+//
+//	
+//
+//
