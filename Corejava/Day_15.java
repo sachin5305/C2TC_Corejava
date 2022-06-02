8c57fc83fc205de56ba449b617afb320fa2eba94
Sachin Singh <sachin11vivaan@gmail.com>
2022-06-02 16:13:44 +0530

    forth commit

---
+++ Corejava/Day_15.java
@@ -0,0 +1,9 @@
+
+public class Day_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day/src/day/Array.java
@@ -0,0 +1,19 @@
+package day;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     int [] arr = new int [] {1, 2, 3, 4, 5};  
+	     int sum = 0;
+	     for(int i = 0; i<=arr.length; i++)
+	     {
+			//sum = sum+i;
+	    	 sum += i;
+
+	     }
+	     	System.out.println("sum=" +sum);
+	}
+		
+		}
+
