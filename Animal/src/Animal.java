31195ffe70684051b9e66a368325dcfe585286b1
Sachin Singh <sachin11vivaan@gmail.com>
2022-06-03 12:48:23 +0530

    fifth commit

---
+++ Animal/src/Animal.java
@@ -0,0 +1,32 @@
+
+public class Animal {
+	void eat()
+	{
+System.out.println("eating...");
+}  
+	}  
+class Dog extends Animal{  
+void bark()
+{
+	System.out.println("barking...");}  
+}  
+class Cat extends Animal
+{  
+void meow()
+{
+	System.out.println("meowing...");
+	}  
+}  
+class TestInheritance3{  
+	public static void main(String[] args) {
+		
+	
+		// TODO Auto-generated method stub
+		Cat c=new Cat();  
+		c.meow();  
+		c.eat();  
+
+		}
+	}
+
+
---
+++ interface/src/player.java
@@ -0,0 +1,15 @@
+ interface player {
+int player_id = 10;
+	void display();
+
+}
+class test1 implements player{
+	public void display() {
+		System.out.println("Captain");
+	}
+	public static void main(String[] args) {
+		test1 T =new test1();
+		T.display();
+		System.out.println(player_id);
+	}
+}
